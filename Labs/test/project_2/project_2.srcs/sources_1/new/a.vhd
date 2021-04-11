b8fa08d9c1d77b4ed4c8107dd894d02f285dba5c
janhonkys <xhonky00@vutbr.cz>
2021-04-11 10:53:11 +0200

    o

---
+++ Labs/test/project_2/project_2.srcs/sources_1/new/a.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 10.04.2021 19:37:12
+-- Design Name: 
+-- Module Name: a - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity a is
+--  Port ( );
+end a;
+
+architecture Behavioral of a is
+
+begin
+
+
+end Behavioral;
